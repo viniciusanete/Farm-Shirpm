0bcf821b546bef8123570b850e96c44fb40d4ee6
Hugo Rabock <hugooliv@hotmail.com>
2018-03-25 03:01:04 -0300

    ok

--- SQL/create Tables.sql
+++ SQL/create Tables.sql
@@ -1,17 +1,6 @@
-
---garçom escolhem os 10% 
---adcional garçom 10%
---1 balcão comida
-
---nao precisa custo
---garçom fecha
-
---o garçom ou caixa podem cadastrar um item no momento do pedido
---lancamentos diarios
-
---cover diario;
---entrega 3 reais cada pizza
---entrega: o campo endereço será preechido, e a mesa null;
---para levar: a mesa estará null e o endereço tb
-
-bugs - pedido gravando pedidosItens desnecessários;
+﻿create table usuarios (
+ id bigint primary key,
+ username varchar(255),
+ password varchar(255),
+ perfil int
+)
