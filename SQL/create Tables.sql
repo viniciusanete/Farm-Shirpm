f916b0cfd361c3654811072ab190930b2f1a0d1b
Hugo Rabock <hugooliv@hotmail.com>
2018-03-25 02:25:56 -0300

    implementando login

---
+++ SQL/create Tables.sql
@@ -0,0 +1,17 @@
+
+--garçom escolhem os 10% 
+--adcional garçom 10%
+--1 balcão comida
+
+--nao precisa custo
+--garçom fecha
+
+--o garçom ou caixa podem cadastrar um item no momento do pedido
+--lancamentos diarios
+
+--cover diario;
+--entrega 3 reais cada pizza
+--entrega: o campo endereço será preechido, e a mesa null;
+--para levar: a mesa estará null e o endereço tb
+
+bugs - pedido gravando pedidosItens desnecessários;
