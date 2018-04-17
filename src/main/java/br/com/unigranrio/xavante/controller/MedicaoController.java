853a0e44d77e182db905c206aa6444f15942276e
Hugo Rabock <hugorabock@nasajon.com.br>
2018-04-17 03:53:11 -0300

    testando uml

--- src/main/java/br/com/unigranrio/xavante/controller/MedicaoController.java
+++ src/main/java/br/com/unigranrio/xavante/controller/MedicaoController.java
@@ -1,8 +1,11 @@
 package br.com.unigranrio.xavante.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.unigranrio.xavante.model.Medicao;
@@ -12,7 +15,8 @@ import br.com.unigranrio.xavante.model.Medicao;
 @RequestMapping(value="/auth/medicao")
 public class MedicaoController {
 
-//	public ResponseEntity receberMedicao(Medicao medicao) {
-//		
-//	}
+	@RequestMapping(method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity receberMedicao(@ResponseBody Medicao medicao) {
+		
+	}
 }
