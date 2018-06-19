0c069bf80cf7d3c641f70287c998a1c3d881f28e
Hugo Rabock <hugorabock@nasajon.com.br>
2018-06-18 21:27:09 -0300

    cross

--- src/main/java/br/com/unigranrio/xavante/controller/ArduinoController.java
+++ src/main/java/br/com/unigranrio/xavante/controller/ArduinoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +23,7 @@ import io.swagger.annotations.ApiOperation;
 
 @Api(value="Api para manipulação do arduino")
 @RestController
+@CrossOrigin("*")
 @RequestMapping(value="/auth/arduino")
 public class ArduinoController {
 
